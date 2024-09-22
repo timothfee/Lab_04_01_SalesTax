10bc8b9497e86b7fc54b5703f76f0aff22b82ba3
Timothy Fee <keys333@proton.me>
2024-09-22 18:50:56 -0400

    Init commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+   public static void main(String[] args) {
+
+    double price = 73.52;
+    double taxRate = 0.2;
+    double salesTax = price * taxRate;
+
+    System.out.println("Price: " + price);
+    System.out.println("Sales Tax: " + salesTax);
+  }
+}
