60ef8f950570031833becb494a965cb7d97e7499
Timothy Fee <keys333@proton.me>
2024-09-29 21:22:48 -0400

    Init commit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
    public static void main(String[] args) {
+    // Ask user for their birth month, if month is between 1 and 12 then display the month, else display error message.
+    Scanner in = new Scanner(System.in);
+    int month = 0;
 
-    double price = 73.52;
-    double taxRate = 0.2;
-    double salesTax = price * taxRate;
+    System.out.print("Enter your birth month between 1 and 12: ");
+    month = in.nextInt();
 
-    System.out.println("Price: " + price);
-    System.out.println("Sales Tax: " + salesTax);
-  }
+    if (month >= 1 && month <= 12) {
+      System.out.println("Your birth month is: " + month);
+    } 
+    else {
+      System.out.println("You entered an invalid month value: " + month);
+    }
+
+   }
 }
